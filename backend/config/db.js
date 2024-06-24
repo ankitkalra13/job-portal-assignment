582ee3daa52a07b20b67775addfaed1aade586a7
ankitkalra13 <68689650+ankitkalra13@users.noreply.github.com>
2024-06-25 03:48:32 +0530

    Delete backend directory

--- backend/config/db.js
+++
@@ -1,9 +0,0 @@
-require ('dotenv').config()
-
-const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI, 
-    console.log("Connected to MongoDB")
-).catch((err) => {
-    console.log(err)
-});
-module.exports = mongoose
